8e3e00436feba382ece0a28e0e1d7dccab7948b4
Spotky1004 <spotky1004@gmail.com>
2020-05-09 06:37:15 +0900

    0 (1)

---
+++ index.js
@@ -0,0 +1,32 @@
+$(function (){
+  gunpowder = new Decimal('1');
+  notationMod = 0;
+
+  function notation(num) {
+    if (notationMod == 0) {
+      if (num.exponent >= 6) {
+        return ((num.mantissa).toFixed(2)) + 'e' + num.exponent;
+      } else {
+        return (num.mantissa*(10**num.exponent)).toFixed(2);
+      }
+    }
+  }
+  function displayBasic() {
+    $('#gunpowder').html(function (index,html) {
+      return 'You Have ' + notation(gunpowder) + ' Gunpowder';
+    });
+  }
+
+  $(document).on('click','#mainNavs > span',function() {
+    indexThis = $('#mainNavs > span').index(this);
+    $('#warpMenus > div').hide();
+    $('#warpMenus > div:eq(' + indexThis + ')').show();
+  });
+
+  setInterval( function (){
+    gunpowder = gunpowder.multiply('12450918243562664640');
+    displayBasic();
+  }, 100);
+
+  $('#mainNavs > span:not(.tier1)').hide();
+});
